064144838f4bbfd8031aa77d79c90561d41b603f
Captain7O9 <99330393+Captain7O9@users.noreply.github.com>
2025-02-12 21:07:33 +0100

    Recreate project with npx sv create

--- server/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from .routers import users, authentication, times
-
-router = APIRouter(prefix="/api")
-router.include_router(times.router)
-router.include_router(users.router)
-router.include_router(authentication.router)
